d26a76288b1b54cb8c776963e5f6e3e76babe571
lengyijun <sjtu5140809011@gmail.com>
2025-01-14 17:33:26 +0800

    wip

--- LeanCamCombi/ExtrProbCombi/Chromatic.lean
+++ LeanCamCombi/ExtrProbCombi/Chromatic.lean
@@ -8,23 +8,26 @@ Verify the second example in https://en.wikipedia.org/wiki/Probabilistic_method
 open MeasureTheory ProbabilityTheory IsBernoulliSeq
 open scoped Finset ENNReal NNReal Set.Notation
 
-variable {α Ω : Type*} [DecidableEq α] [MeasurableSpace Ω] {G : Ω → SimpleGraph α} {H : SimpleGraph α} {p : ℝ≥0} (μ : Measure Ω) (hG : IsBinomialRandomGraph G p μ)
-[IsProbabilityMeasure μ] [Fintype α]
-[DecidableRel H.Adj]
+variable {α Ω : Type*} [DecidableEq α] [Fintype α] [MeasurableSpace Ω] {G : Ω → SimpleGraph α} (μ : Measure Ω) [IsProbabilityMeasure μ] {p : ℝ≥0} (hG : IsBinomialRandomGraph G p μ)
+-- {H : SimpleGraph α} [DecidableRel H.Adj]
 
 namespace ErdosRenyi
 
 include hG
 
--- def hh : Fintype {e : Sym2 α | ¬ e.IsDiag} := by sorry
+instance inter_fintype {s : Finset (Sym2 α)}: Fintype ↑({e: Sym2 α | ¬e.IsDiag} ↓∩ ↑s) :=
+Fintype.ofFinset {e : ↑{e: Sym2 α | ¬e.IsDiag} | (e : Sym2 α) ∈ s} (by simp)
 
 -- A graph with an independent set
 -- wrong
-theorem independent_set_prob (independent_set : Finset α) : μ {ω | ∀ x ∈ ({e : Sym2 α | ¬ e.IsDiag} ↓∩ independent_set.sym2), x ∉ ({e : Sym2 α | ¬ e.IsDiag} ↓∩ (G ω).edgeSet) } = (1-p)^(Nat.choose #independent_set 2) := by
+theorem independent_set_prob (independent_set : Finset α) : μ {ω | ∀ x ∈ ({e : Sym2 α | ¬ e.IsDiag} ↓∩ independent_set.sym2).toFinset, x ∉ ({e : Sym2 α | ¬ e.IsDiag} ↓∩ (G ω).edgeSet) } = (1-p)^(Nat.choose #independent_set 2) := by
 
 rw [hG.meas_ne_subset]
+apply congr <;> try rfl
 
--- , Finset.card_sym2]
+simp
+
+-- rw [Finset.card_sym2]
 
 all_goals sorry
 
